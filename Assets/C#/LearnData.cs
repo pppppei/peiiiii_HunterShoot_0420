a26d1873b6eb7ce2b98360eb025b51ece1eff1d5
peiiiii <pei880106@gmail.com>
2022-05-23 20:21:24 +0800

    練習Car腳本

--- Assets/C#/LearnData.cs
+++ Assets/C#/LearnData.cs
@@ -2,19 +2,19 @@
 using UnityEngine;
 
 //註解，藍字保留字、白色名稱、綠色資料類型
-
+//欄位語法: 修飾詞 資料類型 欄位自訂名稱 指定 值 結束符號
 public class LearnData : MonoBehaviour
 {
     //儲存資料方式:欄位field
     //定義整數資料叫hp
-    int hp;
+    int hp = 100;
 
     //int:+-整數
-    //float:+-小數點
-    //string:文字
-    //bool:Y/N
-    int lv;
-    float exp;
-    string playerName;
-    bool hasCureSkill;
+    //float:+-小數點(後面加f)
+    //string:文字(加"")
+    //bool:T/F
+    int lv = 50;
+    float exp = 0.02f;
+    string playerName = "@Pei16";
+    bool hasCureSkill = true;
 }
